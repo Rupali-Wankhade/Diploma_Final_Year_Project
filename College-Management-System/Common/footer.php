e4d05702930a73b22f8f28dce2dfd24c7bbe3356
Rupali-Wankhade <140294784+Rupali-Wankhade@users.noreply.github.com>
2023-09-28 11:05:41 +0530

    Add files via upload

---
+++ College-Management-System/Common/footer.php
@@ -0,0 +1,45 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Manav College</h3>
+            <p>This Project has been made with the help of 
+              my Allah Almighty.
+              The prupose of this project is Learning Computer Languages.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About M K CITY</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>GRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.E. Civil Engineering</a></li>
+              <li><a href="">B..E. Electrical Engineering</a></li>
+              <li><a href="">B.E. Mechanical Engineering</a></li>
+              <li><a href="">B.E. Architecture</a></li>
+              <li><a href="">B.E. Computer Science</a></li>
+              <li><a href="">Bachelors of Web Development</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Manav College of Engineering,
+                  Khamgaon Road NH 6, Vyala, Tal- Balapur,Dist - Akola-444302</p>
+              <p>Telephones: (+91) 8600007637 /8600007635 /8600007625</p>
+              <p>E-mail: principal@manavengineering.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved.2020</p>
+</footer>
